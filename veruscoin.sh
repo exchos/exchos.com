17a8f65591e08c2c3b39c91a1905b1c0060c0c5e
exchos <85468550+exchos@users.noreply.github.com>
2021-06-08 04:33:29 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,6 @@
+#veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u REXzE58YrACGW7QNHigjKZBDtuePQ8Q6k9.exchos1 -p c=VRSC -t 4
